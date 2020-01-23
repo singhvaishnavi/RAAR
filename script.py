c6cb862115158a879815b44643ba3e6465759526
Vaishnavi Singh <vaishnavisingh.1998@gmail.com>
2020-01-23 16:06:09 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,19 @@
+import os
+import flask
+import pickle
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+@app.route('/form')
+def form():
+    return flask.render_template('form.html')
+
+@app.route('/home')
+def home():
+    return flask.render_template('home.html')

0f572e77fd408aa33ab5672fde7e2bad81ce1d5f
vaish15 <vaishnavi.singh.1509@gmail.com>
2020-01-24 10:44:37 +0530

    pages

--- script.py
+++ script.py
@@ -9,10 +9,18 @@ app=Flask(__name__)
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
-@app.route('/form')
+@app.route('/form',methods=['GET','POST'])
 def form():
-    return flask.render_template('form.html')
+    if request.method == 'GET':
+        return flask.render_template('form.html')
+    return flask.render_template('results.html')
 
 @app.route('/home')
 def home():
     return flask.render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+

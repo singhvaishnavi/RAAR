186869b447f1f025d62c47dc8c361845a00d1c53
vaish15 <vaishnavi.singh.1509@gmail.com>
2020-01-25 10:44:12 +0530

    model version1

--- script.py
+++ script.py
@@ -9,15 +9,39 @@ app=Flask(__name__,static_url_path="/static")
 @app.route('/index')
 def index():
     return flask.render_template('index.html')
+"""
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,12)
+    loaded_model = pickle.load(open("etree.pkl","rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+"""
 @app.route('/form',methods=['GET','POST'])
 def form():
+    if request.method == 'POST':
+        return flask.render_template('result.html')
+    return flask.render_template('form.html')
+
+@app.route('/result',methods=['GET','POST'])
+def result():
+
+    """
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)
+    prediction=str(result)
+    """
     if request.method == 'GET':
-        return flask.render_template('form.html')
-    return flask.render_template('results.html')
+        prediction="prediction"
+        return flask.render_template('result.html',prediction=prediction)
+    return flask.render_template('index.html')        
 
 @app.route('/home',methods=['POST','GET'])
 def home():
     return flask.render_template('home.html')
+
 
 
 if __name__ == '__main__':

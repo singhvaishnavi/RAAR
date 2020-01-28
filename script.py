87f3af7ee60b2a28eaa6488839954e261f20a2aa
vaish15 <vaishnavi.singh.1509@gmail.com>
2020-01-28 19:37:44 +0530

    images

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import os
 import flask
 import pickle
 from flask import Flask,render_template,request,url_for
-app=Flask(__name__,static_url_path="/static")
+app=Flask(__name__,static_url_path="/static/")
 
 
 @app.route('/')
